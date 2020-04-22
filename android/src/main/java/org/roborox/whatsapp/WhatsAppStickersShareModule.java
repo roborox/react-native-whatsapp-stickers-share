3dec73874945a3e6ecf23c413869d0c71c5258b2
roborox <dev@roborox.org>
2020-04-22 14:18:05 +0300

    fix(*): fix android package name

--- android/src/main/java/org/roborox/whatsapp/WhatsAppStickersShareModule.java
+++ android/src/main/java/org/roborox/whatsapp/WhatsAppStickersShareModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package org.roborox.whatsapp;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- android/src/main/java/org/roborox/whatsapp/WhatsAppStickersSharePackage.java
+++ android/src/main/java/org/roborox/whatsapp/WhatsAppStickersSharePackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package org.roborox.whatsapp;
 
 import java.util.Arrays;
 import java.util.Collections;
